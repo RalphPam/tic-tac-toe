94085315d2631d770e07d178001315da6fbedb14
Ralph Nest Pamintuan <rnvpamintuan@rpinnotech.com>
2021-03-24 21:52:49 +0800

    Setup mongodb

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const http = require('http');
+const connectDB = require('./config/db');
 
 const app = express();
 const server = http.createServer(app);
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
