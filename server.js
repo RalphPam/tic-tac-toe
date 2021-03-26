8b11859ea14dfc2b900b10533dc5de61ff6247d9
Ralph Nest Pamintuan <rnvpamintuan@rpinnotech.com>
2021-03-26 18:56:09 +0800

    Configure server to user client build

--- server.js
+++ server.js
@@ -17,6 +17,13 @@ app.use(express.json({ extended: false }));
 app.use('/player', playerRoutes);
 app.use('/room', roomRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+ }
+
 const PORT = process.env.PORT || 5000;
 
 io.on("connection", socket => {
